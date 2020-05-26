1506fa37fb781bc799edb5be904ee39232212994
Albert <acastemoreno@gmail.com>
2020-05-26 11:14:52 -0500

    Review and Game validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,13 @@
 class Review < ApplicationRecord
+  ##Associations
   belongs_to :user
   belongs_to :reviewable, polymorphic: true
 
+  ##Validations
+  validates :title, :body, presence: true
+  validates :title, length: { maximum: 40 }, uniqueness: true
+
+  ##Callback
   after_create Proc.new{|review| up_review_count_of_user_owner(review)}
   after_destroy Proc.new{|review| down_review_count_of_user_owner(review)}
 
